7883a79a166da83829a07ace690f0fb01892a303
jhaelMVP <jhaelmicheldp@gmail.com>
2025-08-11 12:57:33 -0400

    subiendo cambios

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("ahora si")
+window.onload = function boton1() {
+    const boton = document.getElementById("mi_boton");
+    boton.addEventListener("click", function() {
+        alert("presionaste el boton");
+    });
+};
